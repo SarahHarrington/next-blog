b8a51d0a58014fc54f0639587ad71b80974bd685
Sarah Harrington <sarah@sarahdoes.tech>
2023-11-07 15:56:07 -0600

    update env variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,15 +5,16 @@ import {schemaTypes} from './schemas'
 import {codeInput} from '@sanity/code-input'
 import {theme} from 'https://themer.sanity.build/api/hues?default=dfdedf;darkest:19091c&primary=f1dfff'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
 export default defineConfig({
-  name: 'default',
-  title: 'sarahdoestech blog',
-
-  projectId: 'rhfs9aiz',
-  dataset: 'production',
-
+  basePath: '/studio',
+  name: 'Sarah_Does_Tech_Content_Blog',
+  title: 'SarahDoesTech Blog',
+  projectId,
+  dataset,
   plugins: [deskTool(), visionTool(), codeInput()],
-
   schema: {
     types: schemaTypes,
   },
